0f008abffb4ec97f171eb48e68f89a0e41dba973
diegomustdie <diego.frias@artek.edu.mx>
2023-08-27 20:41:01 -0600

    funcionan los npc, falta agregar lugares a todos los Rooms (aun falta bug cuartos)

--- Assets/Scripts/Follower.cs
+++ Assets/Scripts/Follower.cs
@@ -3,20 +3,40 @@ using UnityEngine.AI;
 
 public class Follower : MonoBehaviour
 {
-    public float economyRate = 1f; 
+    private int destinationPoint = 0;
+    private NavMeshAgent agent;
+    private Room currentRoom;
+
+    public float economyRate = 1f;
     public SectStates stateManager;
 
     private float currentEconomy = 0f;
-    public static float globalEconomyRate = 0f;
-
 
     void Start()
     {
+        agent = GetComponent<NavMeshAgent>();
+        agent.autoBraking = false;
+
         stateManager.ChangeFollowerState(SectStates.FollowerState.GenerateEconomy);
+
+        currentRoom = GetComponentInParent<Room>();
+        if (currentRoom != null)
+        {
+            GotoNextPoint();
+        }
+        else
+        {
+            Debug.LogError("Follower is not placed in a room!");
+        }
     }
 
     void Update()
     {
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+        {
+            GotoNextPoint();
+        }
+
         switch (stateManager.GetFollowerState())
         {
             case SectStates.FollowerState.GenerateEconomy:
@@ -31,10 +51,18 @@ public class Follower : MonoBehaviour
     void GenerateEconomy()
     {
         currentEconomy += economyRate * Time.deltaTime;
-        //globalEconomyRate += currentEconomy;
-        //currentEconomy = 0;
         Debug.Log(gameObject.name + " generando economía. Total: " + currentEconomy);
+    }
 
+    void GotoNextPoint()
+    {
+        Transform[] points = currentRoom.GetInterestPoints();
+
+        if (points.Length == 0)
+            return;
+
+        agent.destination = points[destinationPoint].position;
+        destinationPoint = (destinationPoint + 1) % points.Length;
     }
 
     void Idle()
--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -3,14 +3,20 @@ using UnityEngine;
 public class Room : MonoBehaviour
 {
     public int maxFollowers = 10;
+    public Transform[] interestPoints;
 
     public int CurrentFollowersCount()
     {
-        return transform.childCount; 
+        return transform.childCount;
     }
 
     public bool CanAddFollower()
     {
         return CurrentFollowersCount() < maxFollowers;
+    }
+
+    public Transform[] GetInterestPoints()
+    {
+        return interestPoints;
     }
 }
